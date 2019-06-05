7e19030004a3b1be972b7a8cec46821cb2947132
Joshua Ho <ho.joshua4@gmail.com>
2019-06-04 17:08:45 -0700

    Completed header for desktop view

--- src/Header/SocialBar.js
+++ src/Header/SocialBar.js
@@ -1,17 +1,50 @@
 import React from 'react'
-
+import SocialBarIcon from './SocialBarIcon';
 
 import styles from './SocialBar.module.css';
 
+import logoLinkedIn from '../images/linkedInLogo_48x48.png';
+import logoLinkedInHover from '../images/linkedInLogoHover_48x48.png';
+import logoGithub from '../images/gitIcon_48x48_white.png';
+import logoGithubHover from '../images/gitIconHover_48x48.png';
+
 class SocialBar extends React.Component{
 
+  state = {
+    logos: [
+      {
+        href: "https://www.linkedin.com/in/joshua-ho-80b6a251/",
+        img: logoLinkedIn,
+        hover: logoLinkedInHover,
+        alt: "Link to Joshua Ho's Linked In account"
+      },
+      {
+        href: "https://github.com/ricecreamdude",
+        img: logoGithub,
+        hover: logoGithubHover,
+        alt: "Link to Joshua Ho's Git Hub account"
+      }
+    ]
+  }
+
   render(){
-    return (
+
+    let logos = (
       <div className={styles.socialBar}>
-        <div>WHOOP</div>
-        <div>BOOP</div>
-        <div>SHOOP</div>
-        Hi I'm a social bar
+        {this.state.logos.map( logo => {
+          return <SocialBarIcon
+            href={logo.href}
+            img={logo.img}
+            hover={logo.hover}
+            alt={logo.alt}
+          />
+        })}
+      </div>
+    )
+
+    return (
+      <div>
+        {logos}
       </div>
     )
   }
